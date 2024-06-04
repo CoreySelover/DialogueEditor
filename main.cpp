e5525e61a3833301728da8db1d89c9a8da7ca263
CoreySelover <42557438+CoreySelover@users.noreply.github.com>
2024-06-04 13:28:02 -0400

    Added pugixml files and a TGUI form.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <SFML/Graphics.hpp>
 #include <TGUI/TGUI.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
+#include "pugixml.hpp"
 
 int main()
 {
-    sf::RenderWindow window{ {800, 600}, "TGUI example - SFML_GRAPHICS backend" };
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Dialogue Editor");
     tgui::Gui gui{ window };
-    gui.mainLoop(); // See below for how to use your own main loop
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            gui.handleEvent(event);
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        gui.draw();
+
+        window.display();
+    }
 
     return 0;
 }
