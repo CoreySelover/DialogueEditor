84f90bea0819ef026e1926190d6ba9b869ebdf79
CoreySelover <42557438+CoreySelover@users.noreply.github.com>
2024-06-04 14:47:34 -0400

    DialogueTree displays dialogue and child text names.

--- main.cpp
+++ main.cpp
@@ -6,25 +6,40 @@
 
 #include "pugixml.hpp"
 
+sf::RenderWindow window(sf::VideoMode(1920, 1080), "Dialogue Editor");
+tgui::Gui gui{ window };
+
 void loadFile(std::string filename)
 {
 	pugi::xml_document doc;
 	pugi::xml_parse_result result = doc.load_file(filename.c_str());
 
-	if (result)
-	{
-		std::cout << "XML file loaded successfully" << std::endl;
-	}
-	else
-	{
-		std::cout << "XML file loading failed" << std::endl;
+    auto tree = gui.get<tgui::TreeView>("DialogueTree");
+
+    if (result) {
+        // load each dialogue
+        for (pugi::xml_node dialogue = doc.child("Dialogue"); dialogue; dialogue = dialogue.next_sibling("Dialogue")) {
+
+            std::string dId = dialogue.attribute("id").as_string();
+            tree->addItem({ dId });
+
+            // text
+            for (pugi::xml_node text = dialogue.child("Text"); text; text = text.next_sibling("Text")) {
+                std::string id = text.attribute("id").as_string();
+                std::string portrait = text.attribute("portrait").as_string();
+                tree->addItem({ dId, id });
+		    }
+        }
+
+        tree->collapseAll();
+    }
+	else {
+		throw std::runtime_error("Failed to load file: " + filename);
 	}
 }
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Dialogue Editor");
-    tgui::Gui gui{ window };
     gui.loadWidgetsFromFile("editor.txt");
 
     tgui::FileDialog::Ptr filePicker = tgui::FileDialog::create("Open file", "Open", true);
@@ -39,7 +54,6 @@ int main()
     tgui::Filesystem::Path fileName;
     std::string fileName2 = "../CrescentTerminal/CrescentTerminal/Assets/Data/dialogue.xml"; // DEBUG
     bool loaded = false;
-
 
     while (window.isOpen())
     {
